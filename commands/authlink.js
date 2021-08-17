f8175dd07a4723039d18151fbac8e1ec45c64478
noxterr <85783430+noxterr@users.noreply.github.com>
2021-08-17 16:30:56 +0200

    Update authlink.js

--- commands/authlink.js
+++ commands/authlink.js
@@ -6,7 +6,8 @@ module.exports = {
         let user = message.author.id
         console.log(message.author.id)
 
-        let userCB = user.toString('base64');
+        let userCB = Buffer.from(user, 'ascii').toString('base64')
+
         console.log(userCB)
 
         message.channel.send("Link to authorize with faceit :" + `https://faceit-oauth.herokuapp.com/?p=${userCB}`)
