5da124a73f83c2b196db5bdec3bcaa2162c2d076
Mirko Dal Fara <mirko.dalfarra@gmail.com>
2021-08-17 16:23:12 +0200

    added author link

--- commands/authlink.js
+++ commands/authlink.js
@@ -3,7 +3,12 @@ module.exports = {
     description : 'command that sends a link',
     execute(message, args){
 
-        message.channel.send("Link to authorize with faceit :" + 'https://faceit-oauth.herokuapp.com/')
-        
+        let user = message.author
+        console.log(message.author)
+
+        let userCB = user.toString('base64');
+        console.log(userCB)
+
+        message.channel.send("Link to authorize with faceit :" + `https://faceit-oauth.herokuapp.com/?p=${userCB}`)
     }
 }
