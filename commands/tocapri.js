e08704862b3c6f361e3ad1d8346727579988b95b
noxter <mirko.dalfarra@gmail.com>
2021-10-08 11:07:03 +0200

    added TOCAPRI command input + admin allowment

---
+++ commands/tocapri.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name : 'tocapri',
+    description : 'command that returns some to capri pro pic',
+    execute(message, args){
+        return message.channel.send("Questo comando verrà abilitato a breve. -cit Dommasone Sbazio")
+    }
+}
+
+//heroku logs --tail
