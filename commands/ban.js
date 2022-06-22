0a1a276244e401c4b2052481156fb269211ec9f5
noxter <mirko.dalfarra@gmail.com>
2022-06-22 23:45:44 +0200

    ban command

--- commands/ban.js
+++ commands/ban.js
@@ -1,15 +1,49 @@
 require('dotenv').config();
-
+const http = require('axios')
 
 module.exports = {
     name : 'ban',
     description : 'command that returns the list of bans attached to a user',
     execute(message, args){
-        // if(args.length==0){
-        //     return message.reply("Why you think this command works if you don't put anything?")
-        // }
+        if(args.length==0){
+             return message.reply("This command requires a FACEIT guid to work. Use -player [name] to get one")
+        }
 
-        message.channel.send('Soon this will work')
+        let banStatus = []
+        const guid = args[0]
+
+        const callApi = async() => {
+            const data = await http.get(`https://api.faceit.com/sheriff/v1/bans/${guid}`)
+
+            if (data && data.data) {
+                data.data.payload.forEach((ban, i) => {
+                    banStatus.push({
+                        nickname: ban.nickname,
+                        reason: ban.reason,
+                        game: ban.game,
+                        starts: ban.starts_at.replace('T', ' '),
+                        ends: ban.ends_at.replace('T', ' ')
+                    })
+                })
+
+                banMessage = '\`\`\`'
+                if (banStatus.length > 0) {
+                    banStatus.forEach((ban) => {
+                        Object.keys(ban).forEach(banKey => {
+                            banMessage += "\n" + banKey + " " + ban[banKey]
+                        })
+                    })
+                    banMessage += '\`\`\`'
+                    message.channel.send(banMessage)
+                } else {
+                    message.channel.send('No bans found')
+                }
+            } else {
+                message.channel.send('No bans found')
+            }
+        }
+
+        callApi()
     }
 }
 
